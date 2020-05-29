d09f6017b1f7e470d5fab072468c7b81d9a9b465
Corey Burns <coreymburns@gmail.com>
2020-05-29 16:11:33 -0400

    added some styles, pokemon component

---
+++ src/pages/pokemon.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+import Layout from "../components/layout"
+import Lotto from "../components/Apps/Lottery"
+import { Heading } from "theme-ui"
+
+export default () => {
+  const [generate, setGenerate] = useState(false)
+  const refreshApp = () => {
+    setGenerate(!generate)
+  }
+  return (
+    <Layout>
+      <Heading>Pokemon Search</Heading>
+      <Text>Fetch Pokemon at build time &</Text>
+      <Text>Fetch more at run time</Text>
+    </Layout>
+  )
+}
