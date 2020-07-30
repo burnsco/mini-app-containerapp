cdb4aff45fdfadeae8e8867b6149be4362d01737
Corey Burns <coreymburns@gmail.com>
2020-07-30 19:03:12 -0400

    fixed the coin flip game

--- src/pages/coin.js
+++ src/pages/coin.js
@@ -6,6 +6,10 @@ import Layout from '../components/Layout'
 import Heads from '../images/heads.png'
 import Tails from '../images/tails.png'
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
 export default function Coin() {
   const [flipping, setFlipping] = useState(false)
   let [flip, setFlip] = useState(false)
@@ -15,19 +19,24 @@ export default function Coin() {
 
   const flipCoin = () => {
     const head = [true, false]
+
     setFlipping(true)
+
     setFlips((flips += 1))
 
-    const result = head[Math.random() * head.length || 0]
+    const result = head[getRandomInt(2)]
 
-    if (result === true) {
+    if (result) {
       setFlip((flip = true))
       setHeads((heads += 1))
-    } else {
+    }
+    if (result === false) {
       setFlip((flip = false))
       setTails((tails += 1))
     }
-    setFlipping(false)
+    setTimeout(() => {
+      setFlipping(false)
+    }, 2000)
   }
 
   return (
@@ -58,7 +67,7 @@ export default function Coin() {
 
         <p sx={{ height: '30%' }}>
           {' '}
-          <button type="button" onClick={() => flipCoin()}>
+          <button disabled={flipping} type="button" onClick={() => flipCoin()}>
             Flip Me!
           </button>
         </p>
